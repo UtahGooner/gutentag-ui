a1437ea163ef446ec7f62f4ac0281ce9e276446e
Steve Montgomery <steve@chums.com>
2022-05-09 08:22:22 -0600

    fix: Chat line header layout

--- src/components/ChatLine.tsx
+++ src/components/ChatLine.tsx
@@ -12,10 +12,12 @@ const ChatLine:React.FC<ChatLine> = ({createdAt, url, tagname, chattext}) => {
     const timestamp = new Date(createdAt || Date.now()).toLocaleString();
     return (
         <div className="chat">
-            <div className="chat-name" title={timestamp}>
-                <TagName tagName={tagname} url={url} />
+            <div className="chat-header">
+                <div className="chat-name" title={timestamp}>
+                    <TagName tagName={tagname} url={url} />
+                </div>
+                <div className="chat-timestamp">{timestamp}</div>
             </div>
-            <div className="chat-timestamp">{timestamp}</div>
             <div className="chat-text">
                 <EmojifiedText chatText={chattext}/>
             </div>
